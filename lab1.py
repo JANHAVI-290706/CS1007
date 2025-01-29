c8d70d808e3af5a1fdf94b301a8e393ed113223f
JANHAVI B <janhavibbca24@rvu.edu.in>
2025-01-29 12:00:25 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,16 +1,11 @@
-def find_largest_and_smallest(arr):
-    if not arr:
-        return None,None#Return None if the array is empty
-    largest=arr[0]
-    smallest=arr[0]
-    for num in arr:
-        if num>largest:
-            largest=num
-        if num<smallest:
-                smallest=num
-    return largest,smallest
-        # Example usage:
-array=[3,5,1,8,-3,7,2]
-largest,smallest=find_largest_and_smallest(array)
-print("Largest element:",largest)
-print("Smallest element:",smallest)
+arr = [10,89,9,56,4,80,8]
+
+max= arr[0]
+min= arr[0]
+for num in arr:
+    if num > max:
+        max= num
+    if num < min:
+        min = num
+print(f"Largest element: {max}")
+print(f"Smallest element:{min}")
