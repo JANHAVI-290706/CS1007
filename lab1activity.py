51cd4ce94f5a145cb581504075b1b8fd903bb921
JANHAVI B <janhavibbca24@rvu.edu.in>
2025-02-05 14:49:00 +0530

    Delete lab1activity.py

--- lab1activity.py
+++
@@ -1,7 +0,0 @@
-arr = [10,89,9,56,4,80,8]
-arr = list(set(arr))
-arr.sort()
-second_smallest = arr[1]
-second_largest = arr[-2]
-print("Second Largest:", second_largest)
-print("Second Smallest:", second_smallest)
